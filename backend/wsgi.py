4a2a3d1bfde594b9801d2fddbf3dbedc9a9c0d9b
NERD <sssagarbro@gmail.com>
2024-09-01 01:56:40 +0530

    fixed CORS

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,7 @@
+from flask_cors import CORS
 from app import app
+
+CORS(app, resources={r"/*": {"origins": "https://playlist-transfer-lovat.vercel.app"}}, supports_credentials=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
