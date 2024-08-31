af33d712e1a375ce5f29637936c54a61d26063c5
NERD <sssagarbro@gmail.com>
2024-09-01 01:59:15 +0530

    Remove stupid vercel analytics

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Analytics } from "@vercel/analytics/react";
 import { useEffect, useState, useRef } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
